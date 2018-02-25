fe6a0d9a096757858b5f5816a8a3a5d2206e8cf8
Javier <jrenteria3@csustan.edu>
2018-02-24 17:46:55 -0800

    versionControlTest
    Version 1.0

--- ArraySTructure.java
+++ ArraySTructure.java
@@ -103,7 +103,7 @@ public class ArrayStructure {
 	
 	if(j != -1){
 		
-		// ADD THE +2 TO FIX SPACING
+		// ADDED THE +2 TO FIX SPACING
 		
 		for(int k = 0; k < ((j*5)+2); k++)System.out.print(" ");
 		
@@ -113,7 +113,7 @@ public class ArrayStructure {
 	
 	if(i != -1){
 		
-		// ADD THE -1 TO FIX SPACING
+		// ADDED THE -1 TO FIX SPACING
 		
 		for(int l = 0; l < (5*(i - j)-1); l++)System.out.print(" ");
 		
